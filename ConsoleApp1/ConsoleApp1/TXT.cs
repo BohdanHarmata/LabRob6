2ddfb4f5b911af72ef13519344e3221d4cd661af
BohdanHarmata <70683703+BohdanHarmata@users.noreply.github.com>
2020-11-25 23:12:58 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/TXT.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class TXT : AbstractHandler
+    {
+        public string fileName { get; set; }
+
+        public string filePath { get; set; }
+
+        public TXT (string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public override void  FileCreation(string filePath)
+        {
+                File.Create(filePath);
+            fileName = Console.ReadLine() + ".txt";
+        }
+
+        public override void FileReduct(string filePath)
+        {
+            File.OpenWrite(filePath);
+        }
+
+        public override void FileOpen(string filePath, string fileName)
+        {
+            File.Open(filePath + fileName, FileMode.Open);
+        }
+    }
+}
